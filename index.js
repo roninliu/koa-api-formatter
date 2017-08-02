5da4f7ec7ab5a77c2a5b1ad8c67ddf09e5616ae1
ronin <roninliu@foxmail.com>
2017-08-02 17:17:12 +0800

    add init code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = {}
+/**
+ *
+ */
+module.exports = formatter;
+
+function formatter({filter} = {}){
+     return async function formatterAPI(ctx,next){
+        await next();
+        console.log(filter);
+        console.log(ctx.body)
+        console.log(2);
+    }
+}
